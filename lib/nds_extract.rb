e975794afc3f6090fff8e8d14fae9287a0547957
Gustave Habimana <ghabiman@gmail.com>
2020-02-14 23:54:54 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -51,6 +51,8 @@ def total_gross(source)
     director_index = 0
     
     while director_index < source.size do
+    total_gross_of_earnings += gross_for_director( source[director_index] )
+
 
 
 
